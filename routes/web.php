638ac2d2924040110cf0adff33fdd79a6673f5a9
Phuong Anh <npa300405@gmail.com>
2026-03-25 16:59:15 +0700

    Hoàn thành chức năng hiển thị danh sách sách và lọc theo thể loại

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
 use Illuminate\Support\Facades\DB;
-=======
-use Illuminate\Support\Facades\DB; 
+use App\Http\Controllers\MovieController;
 
->>>>>>> b3146831a15117fc6a08a2d98eb9b8e54687b83e
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -23,28 +20,25 @@ Route::get('/', function () {
 });
 
 // Route cho thành viên Nguyễn Ngọc Ý Nhi
-Route::get('/nguyenngocynhi', function () {return 'Nguyễn Ngọc Ý Nhi';});
-<<<<<<< HEAD
-
-
+Route::get('/nguyenngocynhi', function () {
+    return 'Nguyễn Ngọc Ý Nhi';
+});
 
 Route::get('/phim-canada', function () {
     // Truy vấn trực tiếp vào bảng chứa dữ liệu phim
-    $movies = DB::table('movie') // Nhớ đổi 'movie' thành tên bảng thực tế của bạn nếu khác nhé
+    $movies = DB::table('movie') 
                 ->where('country_name', 'Canada')
                 ->get();
 
     return view('yeucau5', ['movies' => $movies]);
 });
 
-=======
-use App\Http\Controllers\MovieController;
 
+// Các route cho bài tập sách
 // Route::get('/baitapNhi', [MovieController::class, 'showData']);
-Route::get('/sach','App\Http\Controllers\ViduLayoutController@sach');
+Route::get('/sach', 'App\Http\Controllers\ViduLayoutController@sach');
 
-Route::get("/thongtinsach","App\Http\Controllers\Book@sach");
+Route::get("/thongtinsach", "App\Http\Controllers\Book@sach");
 
-Route::get('/sach/theloai/{id}','App\Http\Controllers\ViduLayoutController@theloai');
-Route::get('/sach/chitiet/{id}','App\Http\Controllers\ViduLayoutController@chitiet');
->>>>>>> b3146831a15117fc6a08a2d98eb9b8e54687b83e
+Route::get('/sach/theloai/{id}', 'App\Http\Controllers\ViduLayoutController@theloai');
+Route::get('/sach/chitiet/{id}', 'App\Http\Controllers\ViduLayoutController@chitiet');
