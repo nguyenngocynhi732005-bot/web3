e690715caeceadfb9d28194deafa22744ea0b317
QuynhAnh123 <traitimbebong111@gmail.com>
2026-03-31 19:03:40 +0700

    Qa code

--- routes/web.php
+++ routes/web.php
@@ -1,43 +1,28 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
- PhuongAnh
- HEAD
 use Illuminate\Support\Facades\DB;
-
-use Illuminate\Support\Facades\DB; 
-
 use App\Http\Controllers\MovieController;
-use Illuminate\Support\Facades\DB;
 
-main
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 */
 
-
 Route::get('/', function () {
     return view('welcome');
 });
 
-PhuongAnh
-// Route cho thành viên Nguyễn Ngọc Ý Nhi
-Route::get('/nguyenngocynhi', function () {return 'Nguyễn Ngọc Ý Nhi';});
-HEAD
-
-Route::get('/huakimngan', function () {
-    return 'Hứa Kim Ngân';
-
-    // Route cho thành viên Nguyễn Ngọc Ý Nhi
-    Route::get('/nguyenngocynhi', function () {
-        return 'Nguyễn Ngọc Ý Nhi';
-    });
+// Thành viên
+Route::get('/nguyenngocynhi', function () {
     return 'Nguyễn Ngọc Ý Nhi';
 });
 
-// Route cho thành viên Hồ Ngọc Quỳnh Anh
+Route::get('/huakimngan', function () {
+    return 'Hứa Kim Ngân';
+});
+
 Route::get('/hongocquynhanh', function () {
     return 'Hồ Ngọc Quỳnh Anh';
 });
@@ -45,15 +30,13 @@ Route::get('/hongocquynhanh', function () {
 Route::get('/tranthingocan', function () {
     return 'Trần Thị Ngọc An';
 });
- main
 
 Route::get('/nguyenphuonganh', function () {
     return 'Nguyễn Phương Anh';
 });
 
-
+// Phim Canada (query DB)
 Route::get('/phim-canada', function () {
-    // Truy vấn trực tiếp vào bảng chứa dữ liệu phim
     $movies = DB::table('movie')
         ->where('country_name', 'Canada')
         ->get();
@@ -61,43 +44,17 @@ Route::get('/phim-canada', function () {
     return view('yeucau5', ['movies' => $movies]);
 });
 
- PhuongAnh
-
-use App\Http\Controllers\MovieController;
-
-// Route::get('/baitapNhi', [MovieController::class, 'showData']);
+// Route sách
 Route::get('/sach','App\Http\Controllers\ViduLayoutController@sach');
-
 Route::get("/thongtinsach","App\Http\Controllers\Book@sach");
-
 Route::get('/sach/theloai/{id}','App\Http\Controllers\ViduLayoutController@theloai');
 Route::get('/sach/chitiet/{id}','App\Http\Controllers\ViduLayoutController@chitiet');
 
-
-
-
-// 7.1. Danh sách các thể loại phim
+// MovieController
 Route::get('/theloai', [MovieController::class, 'getGenres']);
-
-// 7.2. 10 bộ phim có điểm bình chọn cao nhất
 Route::get('/topphim', [MovieController::class, 'getTopRated']);
-
-
-// 7.3: 10 bộ phim doanh thu cao nhất
 Route::get('/top-budget', [MovieController::class, 'topBudget']);
-
-
-// 7.4: Phim dài hơn 120 phút
 Route::get('/long-runtime', [MovieController::class, 'longRuntime']);
-
-//7.5: Phim Canada
 Route::get('/canada', [MovieController::class, 'canada']);
-
-// Route 7.6 - phim Action
-//có bảng
 Route::get('/action', [MovieController::class, 'action']);
-
-
-// 7.7. Phim siêu phẩm: Điểm > 8.0 và Vote > 10000 (Dạng bảng)
-//có bảng
 Route::get('/phimsieupham', [MovieController::class, 'getHighRatedMovies']);
